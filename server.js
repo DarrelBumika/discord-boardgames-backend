77126edabaccf95e13514b6649e58b6e5f15335b
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2026-09-05 21:46:13 +0700

    fix: port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 dotenv.config({ path: "../.env" });
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // Allow express to parse JSON bodies
 app.use(express.json());
